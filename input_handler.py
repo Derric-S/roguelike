6505db2b734b0297ff3dd8c63784224a2819753d
Derric Standifer <31932319+Derric-S@users.noreply.github.com>
2018-10-25 16:18:25 -0700

    Added NUMPAD movement

--- input_handler.py
+++ input_handler.py
@@ -1,22 +1,22 @@
 def handle_keys(user_input):
 	# movement keys
-	key_char = user_input.char
+	key_char = user_input.key
 
-	if user_input.key == 'UP' or key_char == 'k':
+	if user_input.key == 'UP' or key_char == 'k' or key_char == 'KP8':
 		return {'move': (0, -1)}
-	elif user_input.key == 'DOWN' or key_char == 'j':
+	elif user_input.key == 'DOWN' or key_char == 'j' or key_char == 'KP2':
 		return {'move': (0, 1)}
-	elif user_input.key == 'LEFT' or key_char == 'h':
+	elif user_input.key == 'LEFT' or key_char == 'h' or key_char == 'KP4':
 		return {'move': (-1, 0)}
-	elif user_input.key == 'RIGHT' or key_char == 'l':
+	elif user_input.key == 'RIGHT' or key_char == 'l' or key_char == 'KP6':
 		return {'move': (1, 0)}
-	elif key_char == 'y':
+	elif key_char == 'y' or key_char == 'KP7':
 		return {'move': (-1, -1)}
-	elif key_char == 'u':
+	elif key_char == 'u' or key_char == 'KP9':
 		return {'move': (1, -1)}
-	elif key_char == 'b':
+	elif key_char == 'b' or key_char == 'KP1':
 		return {'move': (-1, 1)}
-	elif key_char == 'n':
+	elif key_char == 'n' or key_char == 'KP3':
 		return {'move': (1, 1)}
 
 	# other keys
